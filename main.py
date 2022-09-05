902159a85ae1e65afdb42bc814c86f3b30633375
The Assassin <112874738+Ajesh2@users.noreply.github.com>
2022-09-05 18:58:01 +0530

    An attempt at calculator, discontinued
    title

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print("hello github")
+import math
+inp = ""
+more = True
+def toList(t):
+    inp += str(t)
+operations = ["+","-","*","/"]
+print("For addition type \'+', for subtraction type \'-', for mulitplication type \'*', for division type \'/'")
+print("type \"done\" when done")
+while more:
+    a = input("number")
+    if type(a) == int:
+        toList(a)
+        b = input("operation")
+        if b in operations:
+            toList(b)
+        else:
+            print("invalid operation")
+
+
+    else:
+        print("invalid number")
+    
+    
+
